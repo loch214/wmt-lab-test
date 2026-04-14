a1587f09d08e2618c16c315bafed3af1e836aad3
Lochana Dahanayake <loch214@gmail.com>
2026-04-14 22:12:39 +0530

    Fix typo in error message variable and update styles for error display

--- public/script.js
+++ public/script.js
@@ -1,11 +1,15 @@
-const erorMsg = document.getElementById('errMsg');
+const errorMsg = document.getElementById('errMsg');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const loginForm = document.getElementById('loginForm');
 
 function loginError() {
     if(!email.value || !password.value){
-        erorMsg.innerHTML = 'Please fill all fields.'
+        errorMsg.innerHTML = 'Please fill all fields.'
+        errorMsg.style.color = 'red'
+    }else {
+        errorMsg.innerHTML = 'All fields are filled.'
+        errorMsg.style.color = 'green'
     }
 }
 
